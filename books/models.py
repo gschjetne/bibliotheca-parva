a9560a54aa78273a27b548f27404104f7a928d27
Grim Schjetne <grim@schjetne.se>
2023-07-24 11:44:06 +0200

    Add more metadata, do ISBN validation

--- books/models.py
+++ books/models.py
@@ -1,11 +1,34 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from isbnlib import canonical, is_isbn10, is_isbn13, to_isbn10, to_isbn13
+
+def validate_canonical_isbn(isbn):
+    if isbn != canonical(isbn):
+        raise ValidationError(
+            f"the ISBN '{isbn}' is not in the canonical form: '{canonical(isbn)}'",
+            params={"isbn": isbn}
+        )
+
+def validate_isbn_10(isbn):
+    if not is_isbn10(isbn):
+        raise ValidationError(
+            f"The ISBN '{isbn}' is not a valid ISBN-10",
+            params={"isbn": isbn}
+        )
+
+def validate_isbn_13(isbn):
+    if not is_isbn13(isbn):
+        raise ValidationError(
+            f"The ISBN '{isbn}' is not a valid ISBN-13",
+            params={"isbn": isbn}
+        )
 
 class Person(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=512)
-    birth_year = models.IntegerField()
-    ol_id = models.CharField(max_length=512, blank=True, unique=True)
+    birth_year = models.IntegerField(blank=True, null=True)
+    ol_id = models.CharField(max_length=512, blank=True, null=True, unique=True)
 
     def __str__(self) -> str:
         return f"{self.name} ({self.birth_year})"
@@ -30,10 +53,11 @@ class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=512)
-    subtitle = models.CharField(max_length=512, blank=True)
-    edition_name = models.CharField(max_length=512, blank=True)
-    description = models.TextField(blank=True)
-    isbn_13 = models.CharField(max_length=13, blank=True)
+    subtitle = models.CharField(max_length=512, blank=True, null=True)
+    edition_name = models.CharField(max_length=512, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    isbn_10 = models.CharField(max_length=10, blank=True, null=True, validators=[validate_isbn_10, validate_canonical_isbn])
+    isbn_13 = models.CharField(max_length=13, blank=True, null=True, validators=[validate_isbn_13, validate_canonical_isbn])
     published_by = models.CharField(max_length=512)
     published_place = models.CharField(max_length=512)
     published_year = models.IntegerField()
@@ -46,3 +70,11 @@ class Book(models.Model):
     def __str__(self) -> str:
         author_list = ", ".join([author.__str__() for author in self.authors.all()])
         return f"{author_list}: {self.title} ({self.published_year})"
+    
+    def clean(self) -> None:
+        if self.isbn_13:
+            self.isbn_10 = to_isbn10(self.isbn_13)
+        elif self.is_isbn10:
+            self.isbn_13 = to_isbn13(self.isbn_10)
+            
+        return super().clean()
