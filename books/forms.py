2891107556fbcb3acc983e90f927541ea3f39dfa
Grim Schjetne <grim@schjetne.se>
2023-08-21 23:18:31 +0200

    Improve styling somewhat

--- books/forms.py
+++ books/forms.py
@@ -3,4 +3,4 @@ from django import forms
 from .validators import validate_any_isbn
 
 class NewFromIsbnForm(forms.Form):
-    isbn = forms.CharField(label='ISBN', max_length='13', required=False, validators=[validate_any_isbn])
+    isbn = forms.CharField(label='ISBN', max_length=13, required=False, validators=[validate_any_isbn])
