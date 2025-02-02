c9fc2bb3a91e4caa8bfd94e9c64367f2f1aa3dcd
Grim Schjetne <grim@schjetne.se>
2025-02-02 12:53:29 +0100

    Add login screen

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("search", views.search, name="search"),
+    path("accounts/", include("django.contrib.auth.urls"))
 ]
